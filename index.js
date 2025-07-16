6bbdd5053760565e711e0a05c5728e831fe39ae5
MD-Kayesur <rmdkayesur@gmail.com>
2025-07-17 01:25:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = require("./app");
+
+ 
+
+require("dotenv").config()
+const port = process.env.PORT || 5000
+
+
+
+app.listen(port,()=>{
+    console.log(`server running at the port ${port}`);
+})
+
